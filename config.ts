035fb3c08ec1c640dc4b9d8354322c02b60669e2
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2024-07-30 12:38:39 +0200

    allow empty password

--- config.ts
+++ config.ts
@@ -3,4 +3,6 @@ export const LOCAL_MODE = false // 192.168.100.*
 
 export const passwd = new Map<string, string>()
 
+if (!ENABLE_AUTH) passwd.set('', '')
+
 passwd.set('4ae1d8bfa6bd45d8', 'b4d7-8d453c61c25b')
